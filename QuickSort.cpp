d127087f0e10c6fcb55cda6713072896961edee4
Lenovo <9.3razzinayyuri@gmail.com>
2024-03-29 11:54:57 +0700

    step 13

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -83,6 +83,9 @@ void q_short(int low, int high)
 	//recursive call to shoort the left sub array
 	q_short(low, j - 1); //step 12
 
-	
+	//recursive call to shoort the right sub array
+	q_short(j + 1, high); //step 13
+
+
 }
 
